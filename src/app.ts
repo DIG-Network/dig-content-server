0ca264642012585033c261e720fae730aa553470
Michael Taylor <mtaylor@michaeltaylor.dev>
2024-09-11 21:00:51 -0400

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { storeRoutes } from "./routes";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cookieParser());
+app.use(cors());
 app.use("/", storeRoutes);
 
 app.use((req, res, next) => {
