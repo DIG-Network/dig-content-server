db9df308a85706aa71dfd24a6ee5666d5c85a84d
Don Kackman <dkackman@gmail.com>
2024-10-29 22:38:41 +0000

    move urn parsing into Udi class

--- src/middleware/parseUdi.ts
+++ src/middleware/parseUdi.ts
@@ -5,6 +5,103 @@ import { Udi } from "../utils/udi";
 
 const validChainNames = ["chia"]; // List of valid chain names
 
+// Extracts the UDI from the request URL or referrer
+function getUdi(requestUrl: string, referrer: string): [boolean, Udi | null] {
+  // Check if the request URL contains a valid UDI
+  if (requestUrl) {
+    try {
+      return [false, Udi.fromUrn(requestUrl.replace(/^\/+/, ''))];
+    } catch (error) {
+    }
+  }
+
+  // Check if the referrer URL contains a valid UDI
+  if (referrer) {
+    try {
+      const url = new URL(referrer);
+      if (url.pathname) {
+        let udi = Udi.fromUrn(url.pathname.replace(/^\/+/, ''));
+
+        // now we have gotten the udi from the referrer, but we might have a path part in the request url
+        // so use it to update the udi. The referrer udi might have a reosurce key which is where the request orginated from
+        // like a script importing another script, NOT the resource being requested
+        if (requestUrl) {
+          udi = udi.fromResourceKey(requestUrl.replace(/^\/+/, ''));
+        }
+        // return true because calling code needs to redirect
+        return [true, udi];
+      }
+    } catch (error) {
+    }
+  }
+
+  console.log('No UDI found in path or referrer: path:', requestUrl, 'referrer:', referrer);
+  return [false, null];
+}
+
+export const parseUdi = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // Skip verification if the route is under the .well-known root
+    if (req.originalUrl.startsWith("/.well-known")) {
+      return next();
+    }
+
+    // strip off the query string as that is not part of the udi
+    const [path, queryString] = req.originalUrl.split("?");
+    const referrer = req.get("Referer") || "";
+
+    const [redirect, udi] = getUdi(removeDuplicatePathPart(path), referrer);
+    if (!udi) {
+      return res.status(400).send("Invalid or missing Udi.");
+    }
+
+    // Validate the chainName
+    if (!validChainNames.includes(udi.chainName)) {
+      console.warn("Invalid chain name:", udi.chainName);
+      return res.status(400).send(renderUnknownChainView(udi.storeId, udi.chainName));
+    }
+
+    // Log extracted values
+    console.log(`Extracted urn:`, udi.toUrn());
+
+    if (redirect) {
+      // the referred might be the storeId or it might be a resource within the storeId
+      // like another script - in the code below we parse that and redirect to the storeId
+      const url = new URL(referrer);
+      const redirectUrl = `${url.origin}/${udi.toUrn()}`
+      console.warn(`Redirecting to referrer:`, redirectUrl);
+      return res.redirect(302, redirectUrl);
+    }
+
+    const dataStore = DataStore.from(udi.storeId);
+
+    let rootHash: string | null = udi.rootHash;
+    // If rootHash is missing, fetch the latest one
+    if (!rootHash) {
+      console.log("RootHash omitted, fetching the latest rootHash...");
+      const storeInfo = await dataStore.fetchCoinInfo();
+      rootHash = storeInfo.latestStore.metadata.rootHash.toString("hex");
+    }
+
+    // Attach extracted components to the request object
+    // @ts-ignore
+    req.chainName = udi.chainName;
+    // @ts-ignore
+    req.storeId = udi.storeId;
+    // @ts-ignore
+    req.rootHash = rootHash
+
+    next();
+  } catch (error) {
+    console.error("Error in parseUdi middleware:", error);
+    res.status(500).send("An error occurred while verifying the identifier.");
+  }
+};
+
 function removeDuplicatePathPart(path: string): string {
   // Split the path into segments, ignoring leading/trailing slashes
   const parts = path.split('/').filter(part => part.length > 0);
@@ -30,146 +127,3 @@ function removeDuplicatePathPart(path: string): string {
   // Reconstruct the path with a leading slash
   return modifiedPath;
 }
-
-export const parseUdi = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    // Skip verification if the route is under the .well-known root
-    if (req.originalUrl.startsWith("/.well-known")) {
-      return next();
-    }
-
-    // Extract the path and query string separately
-    const [path, queryString] = req.originalUrl.split("?");
-
-    // Apply removeDuplicatePathPart to the request path
-    const modifiedPath = removeDuplicatePathPart(path);
-
-    const referrer = req.get("Referer") || "";
-
-    let chainName: string | null = null;
-    let storeId: string = "";
-    let rootHash: string | null = null;
-
-    // Use modifiedPath instead of req.originalUrl
-    const pathSegments = modifiedPath.split("/").filter(segment => segment.length > 0);
-
-    // Extract the first path part as the storeId (assumed app identifier)
-    const pathSegment = pathSegments[0] || ""; // Expecting storeId to be the first path segment
-    const originalPathSegments = pathSegments.slice(1); // Remove the first segment, which is the storeId part
-    let appendPath =
-      originalPathSegments.length > 0
-        ? `/${originalPathSegments.join("/")}`
-        : "";
-
-    // Split the pathSegment by periods to extract potential components
-    const parts = pathSegment.split(":");
-
-    if (parts.length === 1 && parts[0].length !== 64) {
-      appendPath = `/${parts[0]}${appendPath}`;
-    }
-
-    if (parts.length === 5) {
-      chainName = parts[2];
-      storeId = parts[3];
-      rootHash = parts[4]; // rootHash provided in the URL   
-    }
-    else if (parts.length === 4) {
-      chainName = parts[2];
-      storeId = parts[3];
-    }
-    else if (parts.length === 3) {
-      chainName = parts[0];
-      storeId = parts[1];
-      rootHash = parts[2]; // rootHash provided in the URL
-    } else if (parts.length === 2) {
-      if (parts[0].length === 64) {
-        storeId = parts[0];
-        rootHash = parts[1]; // rootHash provided in the URL
-      } else {
-        chainName = parts[0];
-        storeId = parts[1];
-      }
-    } else if (parts.length === 1) {
-      storeId = parts[0];
-    }
-
-    // Log extracted values
-    console.log(
-      "Extracted values - Chain Name:",
-      chainName,
-      "Store ID:",
-      storeId,
-      "Root Hash:",
-      rootHash
-    );
-
-    // Validate storeId length
-    if (!storeId || storeId.length !== 64) {
-      if (referrer) {
-        // the referred might be the storeId or it might be a resource within the storeId
-        // like another script - in the code below we parse that and redirect to the storeId
-        const url = new URL(referrer);
-        const pathParts = url.pathname.split('/').filter(part => part.length > 0);
-        const store = pathParts.length > 0 ? `/${pathParts[0]}` : "";
-
-        console.warn(`Invalid storeId [${storeId}], redirecting to referrer:`, `${url.origin}${store}`);
-        return res.redirect(302, `${url.origin}${store}${appendPath}`);
-      }
-
-      return res.status(400).send("Invalid or missing storeId.");
-    }
-
-    const dataStore = DataStore.from(storeId);
-
-    // Early exit: If both chainName and rootHash are missing, fetch rootHash and redirect with both added
-    if (!chainName && !rootHash) {
-      console.log("Both chainName and rootHash omitted, fetching rootHash...");
-      const storeInfo = await dataStore.fetchCoinInfo();
-      rootHash = storeInfo.latestStore.metadata.rootHash.toString("hex");
-
-      const udi = new Udi("chia", storeId, rootHash, `${appendPath}${queryString ? '?' + queryString : ''}`);
-      const redirect = `/${udi.toUrn()}`;
-      console.log("Redirecting to:", redirect);
-      return res.redirect(302, redirect);
-    }
-
-    // If chainName is missing, assume "chia"
-    if (!chainName) {
-      console.log("ChainName omitted, defaulting to 'chia'.");
-      const udi = new Udi("chia", pathSegment, null, `${appendPath}${queryString ? '?' + queryString : ''}`);
-      const redirect = `/${udi.toUrn()}`;
-      console.log("Redirecting to:", redirect);
-      return res.redirect(302, redirect);
-    }
-
-    // Validate the chainName
-    if (!validChainNames.includes(chainName)) {
-      console.warn("Invalid chain name:", chainName);
-      return res.status(400).send(renderUnknownChainView(storeId, chainName));
-    }
-
-    // If rootHash is missing, fetch the latest one
-    if (!rootHash) {
-      console.log("RootHash omitted, fetching the latest rootHash...");
-      const storeInfo = await dataStore.fetchCoinInfo();
-      rootHash = storeInfo.latestStore.metadata.rootHash.toString("hex");
-    }
-
-    // Attach extracted components to the request object
-    // @ts-ignore
-    req.chainName = chainName;
-    // @ts-ignore
-    req.storeId = storeId;
-    // @ts-ignore
-    req.rootHash = rootHash
-
-    next();
-  } catch (error) {
-    console.error("Error in parseUdi middleware:", error);
-    res.status(500).send("An error occurred while verifying the identifier.");
-  }
-};
--- src/utils/udi.ts
+++ src/utils/udi.ts
@@ -19,6 +19,10 @@ class Udi {
         return new Udi(this.chainName, this.storeId, rootHash, this.resourceKey);
     }
 
+    fromResourceKey(resourceKey: string | null): Udi {
+        return new Udi(this.chainName, this.storeId, this.rootHash, resourceKey);
+    }
+
     static fromUrn(urn: string): Udi {
         const parsedUrn = urns.parseURN(urn);
         if (parsedUrn.nid !== Udi.nid) {
@@ -26,19 +30,29 @@ class Udi {
         }
 
         const parts = parsedUrn.nss.split(':');
+        // at a minimum we need chain name and store id
         if (parts.length < 2) {
             throw new Error(`Invalid URN format: ${parsedUrn.nss}`);
         }
+
+        // this is what a nss looks like
+        //"chia:store id:optional_roothash/optional path/resource key"
         const chainName = parts[0];
-        const storeId = parts[1];
+        const storeId = parts[1].split('/')[0]; // need to strip off the optional path component
+
+        // root hash will always be the part after the second :
         let rootHash: string | null = null;
-        let resourceKey: string | null = null;
         if (parts.length > 2) {
-            rootHash = parts[2];
-            if (parts.length > 3) {
-                resourceKey = parts[3];
-            }
+            rootHash = parts[2].split('/')[0]; // need to strip off the optional path component
         }
+
+        // now see if we have a path component which will always follow the first /
+        const pathParts = parsedUrn.nss.split('/');
+        let resourceKey: string | null = null;
+        if (pathParts.length > 1) {
+            resourceKey = pathParts.slice(1).join('/');
+        }
+
         return new Udi(chainName, storeId, rootHash, resourceKey);
     }
 
@@ -47,9 +61,11 @@ class Udi {
         if (this.rootHash !== null) {
             urn += `:${this.rootHash}`;
         }
+
         if (this.resourceKey !== null) {
             urn += `/${this.resourceKey}`;
         }
+
         return urn;
     }
 
